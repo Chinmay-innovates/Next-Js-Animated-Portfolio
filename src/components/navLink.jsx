4e4bc87ad6c9103c63f4ac1f69c4ae7ce07489c7
Chinmay <chinmaygope69@gmail.com>
2024-02-26 10:52:20 +0530

    next animated portfolio

---
+++ src/components/navLink.jsx
@@ -0,0 +1,15 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+function navLink({ link }) {
+  const pathName = usePathname();
+  return (
+    <Link className={`rounded p-1 ${pathName===link.url && "bg-black text-white"}`} href={link.url}>
+      {link.title}
+    </Link>
+  );
+}
+
+export default navLink;
